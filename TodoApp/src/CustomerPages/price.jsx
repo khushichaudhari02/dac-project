deedfaca7e73972021ed8123b553de16c532d0a2
Naveen Jujjavarapu <naveenjujjavarapu@Naveens-MacBook-Pro.local>
2025-01-29 16:22:50 +0530

    navigation done

---
+++ TodoApp/src/CustomerPages/price.jsx
@@ -0,0 +1,15 @@
+
+import CustomerNavbar from '../components/customerNavbar';
+
+import PricingPage from '../Pricing/pricing';
+const CustomerCalPrice = () => {
+  
+  return (
+    <div>
+      <CustomerNavbar/>
+      <PricingPage/>
+    </div>
+  );
+};
+
+export default CustomerCalPrice;
