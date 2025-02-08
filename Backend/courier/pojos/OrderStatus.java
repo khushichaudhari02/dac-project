f1c6219cb7b46637680f6e0b9a7b9f4076376c9b
Khushi Chaudhari <108222669+khushichaudhari02@users.noreply.github.com>
2025-02-08 15:47:32 +0530

    Delete Backend/courier directory

--- Backend/courier/pojos/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.courier.pojos;
-
-public enum OrderStatus {
-	SHIPPED,PLACED,IN_TRANSIT,OUT_FOR_DELIVERY,DELIVERED
-
-}
--- Backend/courier/repository/WarehouseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.courier.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.courier.pojos.Warehouse;
-
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
-
-}
